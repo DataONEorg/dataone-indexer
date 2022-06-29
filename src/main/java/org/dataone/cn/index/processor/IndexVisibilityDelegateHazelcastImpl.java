88b1a351271f2cc5c1a8c2f8e93e383e42c38114
Jing Tao <tao@nceas.ucsb.edu>
2022-06-29 14:57:50 -0700

    Use the resource map from metacat-index.

--- src/main/java/org/dataone/cn/index/processor/IndexVisibilityDelegateHazelcastImpl.java
+++ src/main/java/org/dataone/cn/index/processor/IndexVisibilityDelegateHazelcastImpl.java
@@ -2,8 +2,14 @@ package org.dataone.cn.index.processor;
 
 import org.apache.log4j.Logger;
 import org.dataone.cn.hazelcast.HazelcastClientFactory;
+import org.dataone.cn.indexer.object.ObjectManager;
 import org.dataone.cn.indexer.resourcemap.IndexVisibilityDelegate;
 import org.dataone.cn.indexer.solrhttp.SolrDoc;
+import org.dataone.service.exceptions.InvalidToken;
+import org.dataone.service.exceptions.NotAuthorized;
+import org.dataone.service.exceptions.NotFound;
+import org.dataone.service.exceptions.NotImplemented;
+import org.dataone.service.exceptions.ServiceFailure;
 import org.dataone.service.types.v1.Identifier;
 import org.dataone.service.types.v2.SystemMetadata;
 
@@ -19,7 +25,8 @@ public class IndexVisibilityDelegateHazelcastImpl implements IndexVisibilityDele
         boolean visible = false;
         try {
 
-            SystemMetadata systemMetadata = HazelcastClientFactory.getSystemMetadataMap().get(pid);
+            //SystemMetadata systemMetadata = HazelcastClientFactory.getSystemMetadataMap().get(pid);
+            SystemMetadata systemMetadata = ObjectManager.getInstance().getSystemMetadata(pid.getValue());
             // TODO: Is pid Identifier a SID?
             if (systemMetadata == null) {
                 return true;
@@ -28,7 +35,17 @@ public class IndexVisibilityDelegateHazelcastImpl implements IndexVisibilityDele
                 visible = true;
             }
         } catch (NullPointerException npe) {
-            logger.warn("Could not get visible value for pid: " + pid.getValue());
+            logger.warn("Could not get visible value for pid: " + pid.getValue() + " since " + npe.getMessage());
+        } catch (InvalidToken e) {
+            logger.warn("Could not get visible value for pid: " + pid.getValue() + " since " +e.getMessage());
+        } catch (NotAuthorized e) {
+            logger.warn("Could not get visible value for pid: " + pid.getValue() + " since " + e.getMessage());
+        } catch (NotImplemented e) {
+            logger.warn("Could not get visible value for pid: " + pid.getValue() + " since " +e.getMessage());
+        } catch (ServiceFailure e) {
+            logger.warn("Could not get visible value for pid: " + pid.getValue() + " since " +e.getMessage());
+        } catch (NotFound e) {
+            logger.warn("Could not get visible value for pid: " + pid.getValue() + " since " +e.getMessage());
         }
         return visible;
     }
@@ -36,7 +53,8 @@ public class IndexVisibilityDelegateHazelcastImpl implements IndexVisibilityDele
     public boolean documentExists(Identifier pid) {
         boolean exists = false;
         try {
-            SystemMetadata systemMetadata = HazelcastClientFactory.getSystemMetadataMap().get(pid);
+            //SystemMetadata systemMetadata = HazelcastClientFactory.getSystemMetadataMap().get(pid);
+            SystemMetadata systemMetadata = ObjectManager.getInstance().getSystemMetadata(pid.getValue());
             if (systemMetadata != null) {
                 exists = true;
             } else {
@@ -45,6 +63,16 @@ public class IndexVisibilityDelegateHazelcastImpl implements IndexVisibilityDele
             }
         } catch (NullPointerException npe) {
             logger.warn("Could not get visible value for pid: " + pid.getValue());
+        } catch (InvalidToken e) {
+            logger.warn("Could not get visible value for pid: " + pid.getValue() + " since " +e.getMessage());
+        } catch (NotAuthorized e) {
+            logger.warn("Could not get visible value for pid: " + pid.getValue() + " since " +e.getMessage());
+        } catch (NotImplemented e) {
+            logger.warn("Could not get visible value for pid: " + pid.getValue() + " since " +e.getMessage());
+        } catch (ServiceFailure e) {
+            logger.warn("Could not get visible value for pid: " + pid.getValue() + " since " +e.getMessage());
+        } catch (NotFound e) {
+            logger.warn("Could not get visible value for pid: " + pid.getValue() + " since " +e.getMessage());
         }
         return exists;
     }
