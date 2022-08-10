6378da77ecc6222f112d94ff1bf9495f204f4563
Jing Tao <tao@nceas.ucsb.edu>
2022-08-10 14:04:43 -0700

    Change the pacakge name for IndexWorker.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,5 +6,5 @@ then
     echo "Starting infinite loop, ctrl-c to interrupt..."
     sh -c 'trap "exit" TERM; while true; do sleep 1; done'
 else
-    java -Dlog4j2.formatMsgNoLookups=true -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:+UseSerialGC -cp ./dataone-index-worker-${TAG}-shaded.jar org.dataone.cn.index.IndexWorker
+    java -Dlog4j2.formatMsgNoLookups=true -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:+UseSerialGC -cp ./dataone-index-worker-${TAG}-shaded.jar org.dataone.cn.indexer.IndexWorker
 fi
