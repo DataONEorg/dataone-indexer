530a9ac97a6b22932bb85a310199f4387bd880a2
Matt Jones <gitcode@magisa.org>
2022-10-11 17:59:28 -0800

    Update to use custom entrypoint for SOLR startup.
    Also update zookeeper directly for creating the configset. Still having trouble
    launching the /solrconfig/config-solr.sh and getting it to authenticate properly,
    although it works fine when run manually.

--- helm/config/config-solr.sh
+++ helm/config/config-solr.sh
@@ -9,6 +9,13 @@ export CONFIG='/bitnami/solr/server/solr/configsets'
 cp -R ${CONFIG}/sample_techproducts_configs ${CONFIG}/${SOLR_COLLECTION}
 cp /solrconfig/schema.xml ${CONFIG}/${SOLR_COLLECTION}/conf/
 cp /solrconfig/solrconfig.xml ${CONFIG}/${SOLR_COLLECTION}/conf/
-rm ${CONFIG}/${SOLR_COLLECTION}/conf/managed-schema
+rm -f ${CONFIG}/${SOLR_COLLECTION}/conf/managed-schema
+
+# Delete the default collection that was created
 /opt/bitnami/solr/bin/solr delete -c ${SOLR_COLLECTION}
-/opt/bitnami/solr/bin/solr create_collection -c ${SOLR_COLLECTION} -d ${CONFIG}/${SOLR_COLLECTION}/ -rf ${SOLR_NUMBER_OF_NODES}
+
+# Create a custom SOLR configset in Zookeeper
+/opt/bitnami/solr/bin/solr zk upconfig -n ${SOLR_COLLECTION} -d ${CONFIG}/${SOLR_COLLECTION} -z ${SOLR_ZK_HOSTS}
+
+# Create a collection using that configset
+/opt/bitnami/solr/bin/solr create_collection -c ${SOLR_COLLECTION} -n ${SOLR_COLLECTION} -rf ${SOLR_NUMBER_OF_NODES}
