7d0ddeefd703453e6de89562c393ea14576a187e
Matt Jones <gitcode@magisa.org>
2022-10-20 21:50:08 -0800

    Upgrade solr config script to check for zookeeper and collection.
    Script still needs further guards to check if commands fail or
    if the pod has already been configured. Also, bump SOLR chart to
    newest version.

--- helm/config/config-solr.sh
+++ helm/config/config-solr.sh
@@ -24,6 +24,7 @@ export SOLR_AUTH_TYPE=basic
 export SOLR_AUTHENTICATION_OPTS="-Dbasicauth=${SOLR_ADMIN_USERNAME}:${SOLR_ADMIN_PASSWORD}"
 export CONFIG='/bitnami/solr/server/solr/configsets'
 export SOLR_COLLECTION=dataone_index
+export SOLR_CMD='/opt/bitnami/solr/bin/solr'
 while [ ! -d "${CONFIG}"/sample_techproducts_configs ] ;
 do
       sleep 1
@@ -36,11 +37,25 @@ cp /solrconfig/schema.xml ${CONFIG}/${SOLR_COLLECTION}/conf/
 cp /solrconfig/solrconfig.xml ${CONFIG}/${SOLR_COLLECTION}/conf/
 rm -f ${CONFIG}/${SOLR_COLLECTION}/conf/managed-schema
 
+echo "** Waiting for Zookeeper **" >> ${LOG}
+if ! solr_wait_for_zookeeper; then
+    echo "Zookeeper not detected" >> ${LOG}
+    exit 1
+fi
+echo "** Zookeeper is up **" >> ${LOG}
+
 echo "** Creating configset in Zookeeper... **" >> ${LOG}
 # Create a custom SOLR configset in Zookeeper
-/opt/bitnami/solr/bin/solr zk upconfig -n ${SOLR_COLLECTION} -d ${CONFIG}/${SOLR_COLLECTION} -z ${SOLR_ZK_HOSTS}
+${SOLR_CMD} zk upconfig -n ${SOLR_COLLECTION} -d ${CONFIG}/${SOLR_COLLECTION} -z ${SOLR_ZK_HOSTS}/solr
+${SOLR_CMD} zk ls /configs -z ${SOLR_ZK_HOSTS}/solr >> ${LOG}
 
 # Now create the collection if it doesn't exist
+# http://localhost:8983/solr/admin/collections?action=list
 echo "** Creating collection... **" >> ${LOG}
-solr_create_collection
+if ! solr_collection_exists "$SOLR_COLLECTION"; then
+    ${SOLR_CMD} create_collection -c ${SOLR_COLLECTION} -n ${SOLR_COLLECTION} -replicationFactor ${SOLR_NUMBER_OF_NODES}
+    echo "Collection created" >> ${LOG}
+else
+    echo "Skipping. Collection already exists." >> ${LOG}
+fi
 echo "** Finished postStart. **" >> ${LOG}
