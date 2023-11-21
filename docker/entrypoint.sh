136009e21dd1057cd6f9ae7169e2fe369a479a01
Matthew B <106352182+artntek@users.noreply.github.com>
2023-11-20 16:04:35 -0800

    running as userId instead of username, so runAsNonRoot works correctly

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,5 +6,10 @@ then
     echo "Starting infinite loop, ctrl-c to interrupt..."
     sh -c 'trap "exit" TERM; while true; do sleep 1; done'
 else
-    java -Dlog4j2.formatMsgNoLookups=true -XX:+UnlockExperimentalVMOptions -XX:+UseContainerSupport -XX:+UseSerialGC -cp ./dataone-index-worker-${TAG}-shaded.jar org.dataone.cn.indexer.IndexWorker
+    java  -Dlog4j2.formatMsgNoLookups=true \
+          -XX:+UnlockExperimentalVMOptions \
+          -XX:+UseContainerSupport \
+          -XX:+UseSerialGC \
+          -cp ./dataone-index-worker-${TAG}-shaded.jar \
+          org.dataone.cn.indexer.IndexWorker
 fi
