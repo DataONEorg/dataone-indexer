13a003fe58fcc91981e09ac7b55ffa24c8543a0b
Matthew B <106352182+artntek@users.noreply.github.com>
2024-04-22 14:22:21 -0700

    configmap log4j & explode jar

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,10 +6,12 @@ then
     echo "Starting infinite loop, ctrl-c to interrupt..."
     sh -c 'trap "exit" TERM; while true; do sleep 1; done'
 else
+    jar -xf dataone-index-worker-${TAG}-shaded.jar
+
     java  -Dlog4j2.formatMsgNoLookups=true \
           -XX:+UnlockExperimentalVMOptions \
           -XX:+UseContainerSupport \
           -XX:+UseSerialGC \
-          -cp ./dataone-index-worker-${TAG}-shaded.jar \
+          -cp ./config/:. \
           org.dataone.cn.indexer.IndexWorker
 fi
