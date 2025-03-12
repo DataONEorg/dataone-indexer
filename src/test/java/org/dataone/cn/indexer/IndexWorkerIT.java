bff203beb36db9db2044556db73b78b1a0969322
Jing Tao <tao@nceas.ucsb.edu>
2025-03-12 13:48:26 -0700

    Changed the test code based on the name of a method was changed in the IndexWorker class.

--- src/test/java/org/dataone/cn/indexer/IndexWorkerIT.java
+++ src/test/java/org/dataone/cn/indexer/IndexWorkerIT.java
@@ -66,7 +66,7 @@ public class IndexWorkerIT {
         if (Files.exists(path)) {
             Files.delete(path);
         }
-        worker.startRabbitMQConnectionProbe();
+        worker.startReadinessProbe();
         int index = 0;
         path = Paths.get("readinessprobe");
         while (!Files.exists(path) && index < LIMIT) {
