c1c95333141d8bb3cfca879beaed49afbc76f773
Jing Tao <tao@nceas.ucsb.edu>
2025-05-20 18:45:40 -0700

    Added the code to parse the command line.

--- src/test/python/index_task_sender.py
+++ src/test/python/index_task_sender.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 import pika
 import sys
+arguments = sys.argv[1:]
+if len(arguments) == 2:
+    print("Arguments:", arguments)
+    id = arguments[0]
+    doc_id = arguments[1]
+else:
+    print("Usage: python3 index_task_sender.py pid docId")
+    sys.exit()
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
@@ -10,14 +18,14 @@ channel.queue_declare(queue='index', durable=True, arguments={'x-max-priority': 
 channel.queue_bind(exchange='dataone-index',
                    queue='index',
                    routing_key='index')
-properties = pika.BasicProperties(headers={'index_type': 'create', 'id': sys.argv[1:], 'docId': sys.argv[2:]})
+headers={'index_type': 'create', 'id': id, 'doc_id': doc_id}
+properties = pika.BasicProperties(headers=headers)
 message = ''
 channel.basic_publish(
     exchange='dataone-index',
     routing_key='index',
     body=message,
-    properties=pika.BasicProperties(
-        delivery_mode=pika.DeliveryMode.Persistent
-    ))
+    properties=properties
+    )
 print(f" [x] Sent {message}")
 connection.close()
