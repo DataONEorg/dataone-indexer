5c02cf28f543196e88aad8ffd4676f930da378ac
Matthew B <106352182+artntek@users.noreply.github.com>
2025-07-14 12:07:59 -0700

    remove obsolete cmd line params. add IDX_JAVA_OPTS var awareness

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 figlet -ck DataONE Indexer
 
-if [[ "$DEBUG" == "TRUE" ]]
-then
+if [[ "$DEBUG" == "TRUE" ]]; then
     echo "Starting infinite loop, ctrl-c to interrupt..."
     sh -c 'trap "exit" TERM; while true; do sleep 1; done'
 else
     echo "Starting DataONE Indexer from jar file: ./dataone-index-worker-${TAG}-shaded.jar,"
-    echo "using memory options: [${IDX_JAVA_MEM}]"
-
-    java  -Dlog4j2.formatMsgNoLookups=true \
-          -XX:+UnlockExperimentalVMOptions \
-          -XX:+UseContainerSupport  $IDX_JAVA_MEM \
-          -XX:+UseSerialGC \
-          -cp /etc/dataone/:./dataone-index-worker-${TAG}-shaded.jar \
+    echo "using command-line options for JAVA_MEM: [${IDX_JAVA_MEM}] and JAVA_OPTS: [${JAVA_OPTS}]"
+    if [[ "$(echo "$IDX_JMX_ENABLED" | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
+        jmx_port=$(echo "$IDX_JAVA_OPTS" | \
+            sed -n 's/.*-Dcom\.sun\.management\.jmxremote\.port=\([0-9]*\).*/\1/p')
+        echo "Connect to JMX via port-forwarding to your localhost, from this pod's port: $jmx_port"
+    fi
+    java  $IDX_JAVA_MEM $IDX_JAVA_OPTS -cp /etc/dataone/:./dataone-index-worker-${TAG}-shaded.jar \
           org.dataone.cn.indexer.IndexWorker
 fi
